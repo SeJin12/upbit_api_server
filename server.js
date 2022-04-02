e5bd944c471cc79b071bed0c06cb9eaae7cbbd72
SeJin12 <tpwls8122@naver.com>
2022-04-03 01:29:21 +0900

    update & add API

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // const asset = require('./asset')
 const express = require('express')
 const fs =require('fs')
+const bodyParser = require('body-parser')
 
 // 
 const request = require('request')
@@ -18,6 +19,9 @@ const server_url = env.UPBIT_OPEN_API_SERVER_URL
 
 // Server
 const app = express()
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(req, res) {
     res.send("upbit API Server!")
@@ -79,12 +83,22 @@ app.get('/v1/api_keys', function(req, res) {
 
 /*
     출금 리스트 조회
+    currency, uuids, txids, limit, page, order_by
+    출금 상태 (state)
+        - submitting : 처리 중
+        - submitted : 처리 완료
+        - almost_accepted : 출금대기중
+        - rejected : 거부
+        - accepted : 승인됨
+        - processing : 처리 중
+        - done : 완료
+        - canceled : 취소됨
 */
-app.get('/v1/withdraws/:currency', function(req, res) {
+app.post('/v1/withdraws', function(req, res) {
     console.log( req.url + ", TIME : " + (new Date())) 
 
-    const currency = req.params.currency
-    const state = 'done'
+    const currency = req.body.currency
+    const state = req.body.state
     const txids = [
       
     ]
@@ -130,6 +144,97 @@ app.get('/v1/withdraws/:currency', function(req, res) {
         res.send(JSON.parse(body))
     })
 })
+
+/*
+    입금 리스트 조회
+    currency, uuids, txids, limit, page, order_by(asc/desc)
+    입금 상태 (state)
+        - submitting : 처리 중
+        - submitted : 처리완료
+        - almost_accepted : 입금 대기 중
+        - rejected : 거절
+        - accepted : 승인됨
+        - processing : 처리 중
+*/
+app.post('/v1/deposits', function(req, res) {
+    console.log( req.url + ", TIME : " + (new Date())) 
+
+    const currency = req.body.currency
+    const state = req.body.state
+    const txids = [
+      
+    ]
+
+    const non_array_body = {
+        currency: currency,
+        state: state,
+    }
+    const array_body = {
+        txids: txids,
+    }
+    const body = {
+        ...non_array_body,
+        ...array_body
+    }
+
+    const txid_query = txids.map(txid => `txids[]=${txid}`).join('&')
+    const query = queryEncode(non_array_body) + '&' + txid_query
+
+    const hash = crypto.createHash('sha512')
+    const queryHash = hash.update(query, 'utf-8').digest('hex')
+
+    const payload = {
+        access_key: access_key,
+        nonce: uuidv4(),
+        query_hash: queryHash,
+        query_hash_alg: 'SHA512',
+    }
+
+    let options = {
+        method: "GET",
+        url: server_url + "/v1/deposits?" + query,
+        headers: {Authorization: `Bearer ${sign(payload, secret_key)}`,
+        json: body},
+    }
+
+
+    request(options, (error, response, body) => {
+        if (error) throw new Error(error)
+    
+        let statusCode = response.statusCode
+        console.log(statusCode)
+        res.send(JSON.parse(body))
+    })
+})
+
+
+/*
+    입출금 현황
+    입출금 현황 및 블록 상태를 조회합니다.
+*/
+app.get('/v1/status/wallet', function(req, res) {
+    console.log( req.url + ", TIME : " + (new Date())) 
+    
+    const payload = {
+        access_key: access_key,
+        nonce: uuidv4(),
+    }
+
+    let options = {
+        method: "GET",
+        url: server_url + '/v1/status/wallet',
+        headers: {Authorization: `Bearer ${sign(payload, secret_key)}`},
+    }
+
+    request(options, (error, response, body) => {
+        if (error) throw new Error(error)
+    
+        let statusCode = response.statusCode
+        console.log(statusCode)
+        res.send(JSON.parse(body))
+    })
+})
+
 
 app.listen(3000, function() {
     console.log("start! node server");
